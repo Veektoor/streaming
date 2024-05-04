b0b22ddcdd879050938dba4fb9949a914f8b445a
Veektoor <gichuhivictorm@gmail.com>
2024-05-04 18:26:21 +0300

    django-heroku changes

--- streaming/settings.py
+++ streaming/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -156,3 +158,6 @@ MEDIA_URL='/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK='bootstrap4'
+
+
+django_heroku.settings(locals())
